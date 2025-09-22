a3533fc453f3b873d9281daca9a994923f541300
keane <keane.adjobi@epitech.eu>
2025-09-22 15:19:57 +0200

    feat(frontend): Background animation to be completed

---
+++ area_front/src/components/ShootingStars.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-stars" aria-hidden="true"></div>
+  <div class="shooting-star" aria-hidden="true"></div>
+  <div class="shooting-star" aria-hidden="true"></div>
+  <div class="shooting-star" aria-hidden="true"></div>
+  <div class="shooting-star" aria-hidden="true"></div>
+</template>
+
+<!-- fonctionne pas encore -->
