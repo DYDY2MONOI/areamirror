8b9420f76418877fe8409cb1f4df7c9512942a28
dylan <dylan.ta@epitech.eu>
2025-09-23 19:53:27 +0200

    feat(mobile): user was always disconnected

--- area-mobile/area/Models/User.swift
+++ area-mobile/area/Models/User.swift
@@ -49,3 +49,17 @@ struct RegisterRequest: Codable {
         case lastName = "last_name"
     }
 }
+
+struct ProfileUpdateRequest: Codable {
+    let firstName: String?
+    let lastName: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case firstName = "first_name"
+        case lastName = "last_name"
+    }
+}
+
+struct ProfileResponse: Codable {
+    let user: User
+}
