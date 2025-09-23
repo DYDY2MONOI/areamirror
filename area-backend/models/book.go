efc2b23baaafab5c977f18fb026fd829c247036a
dylan <dylan.ta@epitech.eu>
2025-09-23 18:39:20 +0200

    feat(back): add new column to user table

--- area-backend/models/book.go
+++ area-backend/models/book.go
@@ -15,6 +15,14 @@ type User struct {
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 
+	Phone              *string     `json:"phone" gorm:"uniqueIndex"`
+	Birthday           *time.Time  `json:"birthday"`
+	Gender             string      `json:"gender"`
+	Country            string      `json:"country"`
+	Lang               string      `json:"lang" gorm:"default:'fr'"`
+	PasswordResetToken *string     `json:"-" gorm:"index"`
+	LoginProvider      string      `json:"login_provider" gorm:"default:'email'"`
+
 	Areas []Area `json:"areas,omitempty" gorm:"foreignKey:UserID"`
 }
 
