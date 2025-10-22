276330398896f9eee58dc3ac93db47c8a2dac859
YlanCuvier <ylan.cuvier@epitech.eu>
2025-10-22 15:12:50 +0200

    feat(frontend): improved the size of area cards

--- area_front/src/views/LandingPage.vue
+++ area_front/src/views/LandingPage.vue
@@ -2807,14 +2807,14 @@ body.modal-open {
 
 .cards-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-template-columns: repeat(auto-fill, 400px);
   gap: 2rem;
   margin-top: 2rem;
-  justify-items: center;
+  justify-content: center;
 }
 
 .area-glare-card {
-  width: 320px;
+  width: 400px;
   height: 420px;
   cursor: pointer;
   transition: transform 0.2s ease;
