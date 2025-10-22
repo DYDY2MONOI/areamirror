c783c68a0a7391928cb3844eb72dc6a3790087a6
Aymen <abidaymen75@gmail.com>
2025-10-22 18:06:34 +0200

    feat(back): implemenatio of a random state for security purpose with oauth2

--- area-backend/controllers/onedrive.go
+++ area-backend/controllers/onedrive.go
@@ -340,3 +340,9 @@ func UnlinkOneDriveAccount(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "OneDrive account unlinked successfully"})
 }
+
+func generateRandomState() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.URLEncoding.EncodeToString(b)
+}
